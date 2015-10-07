1608dce97aa8db558a217dd44eb7d06e5b28d36f
Victor <bulddozer86@gmail.com>
2015-10-07 17:58:51 +0200

    added registration new user

--- KoalaCode.BL/Controllers/AuthorizationController.cs
+++ KoalaCode.BL/Controllers/AuthorizationController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DevOne.Security.Cryptography.BCrypt;
+using KoalaCode.BL.Areas.Admin.Models.User;
 using KoalaCode.BL.Infrastructure.Authorize;
+using KoalaCode.DAL.KoalaCodeDB.Entities;
 using KoalaCode.DAL.KoalaCodeDB.Infrastructure.Data;
 
 namespace KoalaCode.BL.Controllers
@@ -32,34 +34,69 @@ namespace KoalaCode.BL.Controllers
         [HttpPost]
         public ActionResult LogIn(string login, string password)
         {
+            string msg = "There is wrong login or password. Try again.";
+
             if (ModelState.IsValid)
             {
                 var user = UnitOfWork.Users.GetByLogin(login);
 
                 if (user == null)
                 {
-                    ModelState.AddModelError(String.Empty, "There is wrong login or password. Try again.");
+                    ModelState.AddModelError(String.Empty, msg);
                     return View();
                 }
 
                 if (BCryptHelper.CheckPassword(password, user.Password))
                 {
                     UserData.SetUserInfo(user);
-                    return RedirectToAction("Index","Home");
+                    return RedirectToAction("Index", "Home");
                 }
 
-                ModelState.AddModelError(String.Empty, "There is wrong login or password. Try again.");
+                ModelState.AddModelError(String.Empty, msg);
                 return View();
             }
 
-            ModelState.AddModelError(String.Empty, "There is wrong login or password. Try again.");
+            ModelState.AddModelError(String.Empty, msg);
 
             return View();
         }
 
         public ActionResult LogOut()
+        {   
+            UserData.ClearUserInfo();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(LoginUserInfo model)
+        {
+            //TODO :: I so lazy X_x
+            var userByEmail = UnitOfWork.Users.GetByEmail(model.Email);
+            var userByLogin = UnitOfWork.Users.GetByLogin(model.Login);
+
+            if (userByEmail != null) ModelState.AddModelError("Email", string.Format("Email {0} already in use.", model.Email));
+            if (userByLogin != null) ModelState.AddModelError("Login", string.Format("Email {0} already in use.", model.Login));
+
+            if (!ModelState.IsValid) return View(model);
+
+            var user = new User
+            {
+                CreatedDate = DateTime.Now,
+                UpdatedDate = DateTime.Now,
+                Login       = model.Login,
+                Password    = BCryptHelper.HashPassword(model.Password, BCryptHelper.GenerateSalt(12)),
+                Email       = model.Email
+            };
+
+            UnitOfWork.Users.Add(user);
+            UnitOfWork.SaveChanges();
+            
+            return View("LogIn");
         }
     }
 }
