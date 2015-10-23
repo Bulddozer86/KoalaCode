b781df8457ebe1affbc46fa7d4380ae543b4bbc1
Victor <bulddozer86@gmail.com>
2015-10-23 22:37:53 +0200

    added backend navigation

--- KoalaCode.BL/Code/Infrastructure/Navigation/BackendNavigation.cs
+++ KoalaCode.BL/Code/Infrastructure/Navigation/BackendNavigation.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc.Html;
 using KoalaCode.BL.Attributes;
+using KoalaCode.BL.Infrastructure.Authorize;
 using KoalaCode.BL.Models.Navigation.Backend;
 
 
@@ -13,38 +14,45 @@ namespace KoalaCode.BL.Code.Infrastructure.Navigation
     {
         public object GetBackendNavigation()
         {
-            return new List<object>
+            var user = UserData.GetUserInfo();
+            var menuItems = UserNavigation();
+
+            if (user.Roles.Any(x => user.Roles.Contains(Constans.Roles.Moderator))
+                || user.Roles.Any(x => user.Roles.Contains(Constans.Roles.Admin)))
             {
-                AdminAndModeratorNavigation(),
-                AdminNavigation(),
-                UserNavigation()
-            };
+                menuItems = menuItems.Concat(ModeratorNavigation());
+            }
+
+            if (user.Roles.Any(x => user.Roles.Contains(Constans.Roles.Admin)))
+            {
+                menuItems = menuItems.Concat(AdminNavigation());
+            }
+
+            return menuItems.ToList();
         }
 
-        [AuthorizedUsersOnly(Constans.Roles.Admin)]
-        protected List<BackendStaticNavigationModel> AdminNavigation()
+        
+        protected IEnumerable<BackendStaticNavigationModel> AdminNavigation()
         {
             return new List<BackendStaticNavigationModel>
             {
                 {new BackendStaticNavigationModel { Action = "Index", Controller = "Role", Label = "Roles"}}
             };
         }
-
-        [AuthorizedUsersOnly(Constans.Roles.Admin, Constans.Roles.Moderator)]
-        protected List<BackendStaticNavigationModel> AdminAndModeratorNavigation()
+        
+        protected IEnumerable<BackendStaticNavigationModel> ModeratorNavigation()
+        {
+            return new List<BackendStaticNavigationModel>
+            {
+                {new BackendStaticNavigationModel { Action = "Index", Controller = "User", Label = "Users"}}
+            };
+        }
+        
+        protected IEnumerable<BackendStaticNavigationModel> UserNavigation()
         {
             return new List<BackendStaticNavigationModel>
             {
                 {new BackendStaticNavigationModel { Action = "Index", Controller = "Dashboard", Label = "Dashboard"}},
-                {new BackendStaticNavigationModel { Action = "Index", Controller = "User", Label = "Users"}}
-            };
-        }
-
-        [AuthorizedUsersOnly(Constans.Roles.Admin, Constans.Roles.Moderator, Constans.Roles.User)]
-        protected List<BackendStaticNavigationModel> UserNavigation()
-        {
-            return new List<BackendStaticNavigationModel>
-            {
                {new BackendStaticNavigationModel { Action = "Index", Controller = "UserProfile", Label = "Profile"}},
                {new BackendStaticNavigationModel { Action = "Index", Controller = "Articles", Label = "Articles"}}
             };
